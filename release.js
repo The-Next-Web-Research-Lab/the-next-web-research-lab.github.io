ab9f0a8ed3d0f2c905b8edfc2a75a44d192d0ba5
Yongku Cho <04whdydrn30@gmail.com>
2024-05-12 23:02:04 +0900

    feat: 수정 후 배포

--- release.js
+++ release.js
@@ -5,6 +5,14 @@ try {
 } catch {
     console.error("fs.removeSync('./assets'); 실패");
 }
+
 fs.removeSync("./src");
-fs.copySync("./docs/.vitepress/dist", "./");
-fs.copySync("./docs/.vitepress/public/robots.txt", "./robots.txt");
+
+try {
+  fs.copySync("./docs/.vitepress/dist", "./");
+  fs.copySync("./docs/.vitepress/public/robots.txt", "./robots.txt");
+} catch {
+  console.error(`
+  fs.copySync("./docs/.vitepress/dist", "./");
+  fs.copySync("./docs/.vitepress/public/robots.txt", "./robots.txt"); 실패`);
+}
