ef3b8b1a675e7aa9a3962e5f23bb16ae30189c69
Yongku Cho <04whdydrn30@gmail.com>
2024-08-10 08:50:24 +0900

    feat: 카피라이트 변경

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,54 +1,46 @@
-import {
-  defineConfig
-} from 'vitepress'
+import { defineConfig } from "vitepress";
 
-import {
-  default as head
-} from "./configs/head";
-import {
-  default as themeLocalesEn
-} from "./configs/theme-locales-en";
-import {
-  default as themeLocalesKr
-} from "./configs/theme-locales-kr";
+import { default as head } from "./configs/head";
+import { default as themeLocalesEn } from "./configs/theme-locales-en";
+import { default as themeLocalesKr } from "./configs/theme-locales-kr";
 
 export default defineConfig({
-  lang: 'ko-KR',
+  lang: "ko-KR",
   head,
-  appearance: 'dark',
-  ignoreDeadLinks: 'localhostLinks',
+  appearance: "dark",
+  ignoreDeadLinks: "localhostLinks",
   themeConfig: {
     search: {
-      provider: 'local',
+      provider: "local",
     },
   },
   sitemap: {
-    hostname: 'https://the-next-web-research-lab.github.io',
+    hostname: "https://the-next-web-research-lab.github.io",
   },
   locales: {
     root: {
-      label: '한국어',
-      lang: 'ko',
-      title: '더넥스트웹리서치랩',
-      description: '담백한 프런트엔드 기술 블로그',
+      label: "한국어",
+      lang: "ko",
+      title: "더넥스트웹리서치랩",
+      description: "담백한 프런트엔드 기술 블로그",
       themeConfig: {
         ...themeLocalesKr,
         footer: {
-          copyright: '© 더넥스트웹리서치랩 Corp. All rights reserved.'
+          copyright: "© 2024 더넥스트웹리서치랩 Corp. All rights reserved.",
         },
-      }
+      },
     },
     en: {
-      label: 'English',
-      lang: 'en',
-      title: 'The Next Web Research Lab',
-      description: 'A plain front-end technology blog',
+      label: "English",
+      lang: "en",
+      title: "The Next Web Research Lab",
+      description: "A plain front-end technology blog",
       themeConfig: {
         ...themeLocalesEn,
         footer: {
-          copyright: '© The Next Web Research Lab Corp. All rights reserved.'
+          copyright: "© The Next Web Research Lab Corp. All rights reserved.",
         },
-      }
-    }
+      },
+    },
   },
 });
