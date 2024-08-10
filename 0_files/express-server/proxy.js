5651ee6c688062287c99a4d5ffcc7276a4038b4f
Yongku Cho <04whdydrn30@gmail.com>
2024-08-10 11:24:43 +0900

    feat: 코드정리

--- 0_files/express-server/proxy.js
+++ 0_files/express-server/proxy.js
@@ -1,18 +1,13 @@
-const express = require("express");
 const {
   createProxyMiddleware,
   fixRequestBody,
   responseInterceptor,
 } = require("http-proxy-middleware");
-const router = express.Router();
-
-router.get("/home", (req, res) => {
-  res.status(200).send("더넥스트웹리서치랩");
-});
 
 module.exports = {
   verifyAuth: (req, res, next) => {
     console.log("인증/인가 처리");
+
     next();
   },
   proxy: (req, res, next) => {
